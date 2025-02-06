b589f93c31bc6f6576771cf0b83647e693e0e126
LUAN VICTOR PANIAGUA CASTRO <luan.vpcastro@senacsp.edu.br>
2025-02-06 11:14:58 -0300

    while e do while

---
+++ ex012/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>While e Do While</title>
+</head>
+<body>
+    <?php 
+    //While
+    $i = 1;
+
+    while($i <= 5){
+        echo $i . "<br>"; //imprime o valor de $i
+        $i++; //i+1
+    }
+
+    echo "<br>";
+    //Mostrar somente números pares
+    $f = 2;
+    while($f <=10){
+        echo $f . "<br>";
+        $f += 2; //Atribui de 2 em 2
+    }
+
+    echo "<br>";
+    //Do While
+    $g = 1;
+    do{
+        echo $g . "<br>";
+        $g++;
+    } while ($g <= 7 );
+
+    ?>
+</body>
+</html>
